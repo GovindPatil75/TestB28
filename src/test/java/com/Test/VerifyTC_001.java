4d2abc2f764936cf24299b0dc4368e849c950353
GovindPatil75 <mulegr75@gmail.com>
2024-12-26 09:08:05 +0530

    Demo Test

---
+++ src/test/java/com/Test/VerifyTC_001.java
@@ -0,0 +1,10 @@
+package com.Test;
+
+public class VerifyTC_001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello World");
+	}
+
+}
