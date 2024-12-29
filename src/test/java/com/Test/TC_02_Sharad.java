d34cad73e0bd1a4b50a8deb3f0e36bae973c5392
shw16 <sharadwasre2018@gmail.com>
2024-12-29 10:05:32 +0530

    TC Sharad

---
+++ src/test/java/com/Test/TC_02_Sharad.java
@@ -0,0 +1,13 @@
+package com.Test;
+
+public class TC_02_Sharad {
+
+	public static void main(String[] args) {
+
+		char c = 'A';
+		System.out.println(c);
+		
+		System.out.println("Variable declared and Initilized");
+	}
+
+}
